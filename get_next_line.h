83bb7e4abf1b92d40be0e9567950e66cf04784fe
imahri <115317377+imahri@users.noreply.github.com>
2023-02-13 18:38:07 +0100

    fiddler

---
+++ get_next_line.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imahri <imahri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/31 17:58:37 by imahri            #+#    #+#             */
+/*   Updated: 2022/11/07 09:04:51 by imahri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char const *s);
+
+void	*ft_calloc(size_t count, size_t n);
+
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: imahri <imahri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/05 18:08:29 by imahri            #+#    #+#             */
+/*   Updated: 2022/11/07 09:04:30 by imahri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <unistd.h>
+
+size_t	ft_strlen(char const *s);
+
+void	*ft_calloc(size_t count, size_t n);
+
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(const char *s, int c);
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s1);
+
+#endif
